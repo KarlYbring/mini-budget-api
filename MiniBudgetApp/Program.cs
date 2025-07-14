aa4c987b3b21359400107a5e1285a215765b0d1d
Karl <karl.ybring@hotmail.com>
2025-07-14 18:03:46 +0200

    fixat så att events skapas vid start av backend

--- MiniBudgetApp/Program.cs
+++ MiniBudgetApp/Program.cs
@@ -58,9 +58,24 @@ using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<DataContext>();
     db.Database.Migrate();
+
+    // Seed default categories if they don't exist
+    var defaultCategories = new[]
+    {
+        "Mat", "Transport", "Abonnemang", "Nöjen", "Kläder", "Annat", "Hyra"
+    };
+
+    foreach (var name in defaultCategories)
+    {
+        if (!db.Categories.Any(c => c.Name == name))
+        {
+            db.Categories.Add(new MiniBudgetApp.Models.Category { Name = name });
+        }
+    }
+    db.SaveChanges();
 }
 
-    app.MapOpenApi();
+app.MapOpenApi();
 app.UseCors();
 app.UseHttpsRedirection();
 app.UseAuthentication();
